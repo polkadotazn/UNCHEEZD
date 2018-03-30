8b2affbe22777971803e524f3530527ef4f753c6
Louisa Xue <xuelex@gmail.com>
2018-03-30 09:11:05 -0700

    Add user actions

--- frontend/actions/user_actions.js
+++ frontend/actions/user_actions.js
@@ -1,14 +1,26 @@
 import * as UserAPIUtil from '../util/user_api_util';
 
 export const RECEIVE_ALL_USERS = 'RECEIVE_ALL_USERS';
+export const RECEIVE_USER = 'RECEIVE_USER';
 
 export const receiveAllUsers = users => ({
   type: RECEIVE_ALL_USERS,
   users
 });
 
+export const receiveUser = users => ({
+  type: RECEIVE_USER,
+  users
+});
+
 export const requestAllUsers = () => dispatch => {
   return UserAPIUtil.fetchAllUsers().then(users => {
     return dispatch(receiveAllUsers(users));
+  });
+};
+
+export const requestUser = id => dispatch => {
+  return UserAPIUtil.fetchAUser(id).then(user => {
+    return dispatch(receiveUser(user));
   });
 };
--- frontend/reducers/user_reducer.js
+++ frontend/reducers/user_reducer.js
@@ -1,12 +1,15 @@
 import merge from 'lodash/merge';
 
-import { RECEIVE_ALL_USERS } from '../actions/user_actions';
+import { RECEIVE_ALL_USERS,
+  RECEIVE_USER } from '../actions/user_actions';
 
 const userReducer = (state = {}, action) => {
   Object.freeze(state);
   switch(action.type) {
     case RECEIVE_ALL_USERS:
       return merge({}, state, action.users);
+    case RECEIVE_USER:
+      return merge({}, state, {[action.user.id]: action.user});
     default:
       return state;
   }
