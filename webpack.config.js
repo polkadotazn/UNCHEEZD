af396f486c5dd12700eefa2aa3bd353b02916dbc
polkadotazn <xuelex@gmail.com>
2020-04-23 15:22:40 -0400

    update webpack

--- webpack.config.js
+++ webpack.config.js
@@ -4,7 +4,7 @@ var webpack = require("webpack");
 var plugins = []; // if using any plugins for both dev and production
 var devPlugins = []; // if using any plugins for development
 
-const UglifyJsPlugin = require("uglifyjs-3-webpack-plugin");
+var UglifyJsPlugin = require("uglifyjs-3-webpack-plugin");
 
 var prodPlugins = [
   new webpack.DefinePlugin({
