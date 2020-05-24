ad006b4ebca0f1ad8c4f8fe74c8d44a741034f63
polkadotazn <xuelex@gmail.com>
2020-05-23 20:52:30 -0400

    fix currentUser

--- frontend/components/session_form/session_form.jsx
+++ frontend/components/session_form/session_form.jsx
@@ -34,7 +34,6 @@ class SessionForm extends React.Component {
     e.preventDefault();
     const user = this.state;
     this.props.processForm({user});
-    console.log("process");
     return <Redirect to='/home' />;
   }
 
