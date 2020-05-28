a7868cafb8b027248de4236f63d8ec9efdf4b330
polkadotazn <xuelex@gmail.com>
2020-05-28 12:41:20 -0400

    add favicon to browser tab

--- frontend/components/shared/star_rating_item.jsx
+++ frontend/components/shared/star_rating_item.jsx
@@ -4,7 +4,7 @@ import { Link } from 'react-router-dom';
 
 const StarRatingItem = ({ rating, fractions }) => {
   return (
-    <div class="star-rating-item">
+    <div className="star-rating-item">
       <Rating
         className="star-rating"
         emptySymbol={<img src={window.clearChzImg} className="star" />}
